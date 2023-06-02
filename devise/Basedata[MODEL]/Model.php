03fea8866dd1d2c00d9d943c05a4e88202419f33
bxel <yogipratama.ut@gmail.com>
2023-06-02 21:00:52 +0700

    first commit by yogi

---
+++ devise/Basedata[MODEL]/Model.php
@@ -0,0 +1,9 @@
+<?php 
+
+    namespace xel\framework\Basedata;
+
+    class Model{
+        public function index():void {
+            return;
+        }
+    }
---
+++ setup/base_class/BaseData.php
@@ -0,0 +1,8 @@
+<?php
+    namespace xel\framework\setup\baseclass;
+
+    class baseData {
+        public function __construct(){
+            return;
+        }
+    }
